ce675366c1ecd93f4cfde740809df26896bd4c3e
best0032001 <cheewin.b@cmu.ac.th>
2021-03-29 20:10:18 +0700

    add demo NotiController

---
+++ WorkShopNET5/Controllers/v1/NotiController.cs
@@ -0,0 +1,86 @@
+﻿using ITSC_API_GATEWAY_LIB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WorkShopNET5.Model.Interface;
+
+namespace WorkShopNET5.Controllers.v1
+{
+    [Route("api/")]
+    [ApiController]
+    public class NotiController : ITSCController
+    {
+        public NotiController(IHttpClientFactory clientFactory, ITSCServer iTSCServer, ILogger<ITSCController> logger, IHrStoreRepository hrStoreRepository)
+        {
+            this.loadConfig(logger, iTSCServer, clientFactory);
+
+
+        }
+        [HttpGet("v1/noti")]
+        public async Task<IActionResult> noti()
+        {
+
+            NotiMgsView notiMgsView = new NotiMgsView();
+            notiMgsView.appID = "xxxxxxxxxxxxxxxxxxxxxxxx";
+            notiMgsView.cmuaccount = new List<string>();
+            notiMgsView.cmuaccount.Add("xxxxxxxxx@cmu.ac.th");
+            notiMgsView.message = "test lib 3.1.0";
+
+            List<NotiReturnView> test = await this.NotiService(notiMgsView);
+            foreach (NotiReturnView notiReturnView in test)
+            {
+
+                if (notiReturnView.isEmail)
+                {
+                    // sendEmail  (notiReturnView.cmuaccount)
+                }
+            }
+            return Ok();
+        }
+        [HttpGet("v1/notiNoLib")]
+        public async Task<IActionResult> notiNoLib()
+        {
+            NotiMgsView notiMgsView = new NotiMgsView();
+            notiMgsView.appID = "xxxxxxxxxxxxxxxxxxxxxxxx";
+            notiMgsView.cmuaccount = new List<string>();
+            notiMgsView.cmuaccount.Add("xxxxxxxxx@cmu.ac.th");
+            notiMgsView.message = "test lib 3.1.0";
+
+            String notiAPI = "https://xxxxxxxxxxx/noti/dev/v1/NotiService";
+            HttpClient httpClient = this._clientFactory.CreateClient();
+            var response = await httpClient.PostAsync(notiAPI, new StringContent(JsonConvert.SerializeObject(notiMgsView)));
+            var responseString = await response.Content.ReadAsStringAsync();
+            APIModelNotiDemo dataTemp = JsonConvert.DeserializeObject<APIModelNotiDemo>(responseString);
+            foreach (NotiReturnViewDemo notiReturnView in dataTemp.data)
+            {
+
+                if (notiReturnView.isEmail)
+                {
+                    // sendEmail  (notiReturnView.cmuaccount)
+                }
+            }
+            return Ok();
+        }
+    }
+    public class NotiReturnViewDemo
+    {
+        
+        public string cmuaccount { get; set; }
+        public bool isEmail { get; set; }
+    }
+    public class APIModelNotiDemo
+    {
+     
+
+        public List<NotiReturnViewDemo> data { get; set; }
+        public string message { get; set; }
+    }
+
+}
